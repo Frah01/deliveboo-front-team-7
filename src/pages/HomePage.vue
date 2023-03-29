f87e1de425a5d295cd8792e1e683c9f8f68a4843
giuseppemaisto <gmaisto98@gmail.com>
2023-03-29 15:00:01 +0200

    close #13

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,11 +2,13 @@
 
 import RicercaCat from '../components/RicercaCat.vue';
 import AppJumbo from '../components/AppJumbo.vue';
+import ListaRistoranti from '../components/ListaRistoranti.vue';
 export default {
     name: 'HomePage',
     components:{
         RicercaCat,
-        AppJumbo
+        AppJumbo, 
+        ListaRistoranti,
     }
 }
 </script>
@@ -17,6 +19,7 @@ export default {
             <div class="row" >
                 <div class="col-12 mt-5">
                     <RicercaCat></RicercaCat>
+                    <ListaRistoranti></ListaRistoranti>
                 </div>
             </div>
         </div>
