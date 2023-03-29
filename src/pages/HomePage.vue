b1d107a8a8995ef31f2a1695bda5c01dd57c11c3
dunzolo <rossidavide108@gmail.com>
2023-03-29 14:17:44 +0200

    cambiata posizione pagina piatti

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,27 +2,18 @@
 
 import RicercaCat from '../components/RicercaCat.vue';
 import AppJumbo from '../components/AppJumbo.vue';
-import AppDishes from '../components/AppDishes.vue';
 
 export default {
     name: 'HomePage',
     components: {
         RicercaCat,
-        AppJumbo,
-        AppDishes
+        AppJumbo
     }
 }
 </script>
 <template lang="">
     <AppJumbo></AppJumbo>
     <RicercaCat></RicercaCat>
-    <div class="container">
-        <div class="row" >
-            <div class="col-12 mt-5">
-                <AppDishes/>
-            </div>
-        </div>
-    </div>
 </template>
 <style lang="">
     
