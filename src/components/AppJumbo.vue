410077af65b831510b96fc40486b8a3156542038
Frah01 <vinario.francesco@gmail.com>
2023-03-29 17:32:30 +0200

    Adj width jumbotron

--- src/components/AppJumbo.vue
+++ src/components/AppJumbo.vue
@@ -6,62 +6,62 @@ export default {
 
 <template lang="">
     <div class="jumbotron-deliveroo flex-carosello">
-    <div class="title-jumbo">
-        <h1 >Benvenuto su Deliveboo! Ordina i tuoi piatti preferiti nella città di <strong> Roma </strong>comodamente da casa tua!</h1>
-    </div>
-    <div class="slider">
-        <div class="slide-track">
-            <div class="slide">
-                <img src="https://wallpaperaccess.com/full/826919.jpg" height="100" width="250" alt="" />
-            </div>
-            <div class="slide">
-                <img src="https://wallpaperaccess.com/full/767390.jpg" height="100" width="250" alt="" />
-            </div>
-            <div class="slide">
-                <img src="https://images8.alphacoders.com/103/1032061.jpg" height="100" width="250" alt="" />
-            </div>
-            <div class="slide">
-                <img src="https://images6.alphacoders.com/109/1092902.jpg" height="100" width="250" alt="" />
-            </div>
-            <div class="slide">
-                <img src="https://wallpaperaccess.com/full/767398.jpg" height="100" width="250" alt="" />
-            </div>
-            <div class="slide">
-                <img src="https://wallpaperaccess.com/full/767316.jpg" height="100" width="250" alt="" />
-            </div>
-            <div class="slide">
-                <img src="http://yesofcorsa.com/wp-content/uploads/2019/05/4K-Egg-Breakfast-Wallpaper-For-Desktop.jpg" height="100" width="250" alt="" />
-            </div>
-            <div class="slide">
-                <img src="https://images6.alphacoders.com/910/thumb-1920-910443.jpg" height="100" width="250" alt="" />
-            </div>
-            <div class="slide">
-                <img src="https://backiee.com/static/wpdb/wallpapers/1000x563/181547.jpg" height="100" width="250" alt="" />
-            </div>
-            <div class="slide">
-                <img src="https://wallpaperbat.com/img/420093-food-pizza-wallpaper-hd-desktop-and-mobile-background.jpg" height="100" width="250" alt="" />
-            </div>
-            <div class="slide">
-                <img src="https://tse1.mm.bing.net/th?id=OIP.CfYjDCo7mtHKc3-DT5fooAHaE8&pid=Api" height="100" width="250" alt="" />
-            </div>
-            <div class="slide">
-                <img src="https://c4.wallpaperflare.com/wallpaper/435/472/996/sandwich-4k-for-desktop-wallpaper-preview.jpg" height="100" width="250" alt="" />
-            </div>
-            <div class="slide">
-                <img src="https://tse3.mm.bing.net/th?id=OIP.DVAM4yhTLhylYFYhpAYaEwHaEK&pid=Api" height="100" width="250" alt="" />
-            </div>
-            <div class="slide">
-                <img src="https://c4.wallpaperflare.com/wallpaper/303/793/304/buffet-meal-food-brunch-wallpaper-thumb.jpg" height="100" width="250" alt="" />
+        <div class="title-jumbo">
+            <h1 >Benvenuto su Deliveboo! Ordina i tuoi piatti preferiti nella città di <strong> Roma </strong>comodamente da casa tua!</h1>
+        </div>
+        <div class="slider">
+            <div class="slide-track">
+                <div class="slide">
+                    <img src="https://wallpaperaccess.com/full/826919.jpg" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://wallpaperaccess.com/full/767390.jpg" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://images8.alphacoders.com/103/1032061.jpg" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://images6.alphacoders.com/109/1092902.jpg" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://wallpaperaccess.com/full/767398.jpg" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://wallpaperaccess.com/full/767316.jpg" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="http://yesofcorsa.com/wp-content/uploads/2019/05/4K-Egg-Breakfast-Wallpaper-For-Desktop.jpg" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://images6.alphacoders.com/910/thumb-1920-910443.jpg" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://backiee.com/static/wpdb/wallpapers/1000x563/181547.jpg" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://wallpaperbat.com/img/420093-food-pizza-wallpaper-hd-desktop-and-mobile-background.jpg" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://tse1.mm.bing.net/th?id=OIP.CfYjDCo7mtHKc3-DT5fooAHaE8&pid=Api" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://c4.wallpaperflare.com/wallpaper/435/472/996/sandwich-4k-for-desktop-wallpaper-preview.jpg" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://tse3.mm.bing.net/th?id=OIP.DVAM4yhTLhylYFYhpAYaEwHaEK&pid=Api" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://c4.wallpaperflare.com/wallpaper/303/793/304/buffet-meal-food-brunch-wallpaper-thumb.jpg" height="100" width="250" alt="" />
+                </div>
             </div>
         </div>
     </div>
-</div>
 </template>
 
 <style lang="scss">
 
     .jumbotron-deliveroo {
-    width: 100vw;
+    width: 100%;
     height: 850px;
     background-image: url('https://images8.alphacoders.com/942/thumb-1920-942495.jpg');
     background-position: center;
