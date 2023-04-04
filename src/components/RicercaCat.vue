e5dd35db1a29b9cd6e8b44d357fcd15c907fca6b
alessiam1909 <ale.menon.vr@gmail.com>
2023-04-04 16:49:28 +0200

    modificata colonna categorie

--- src/components/RicercaCat.vue
+++ src/components/RicercaCat.vue
@@ -43,9 +43,9 @@ export default {
 
         <div class="container-fluid backg-color">
             <h2 class="titolo text-center mt-2">Categorie</h2>
-            <div v-for="category in this.categories" :key="category.id">
+            <div v-for="category in this.categories" :key="category.id" class="badge-div">
                 <button type="button" 
-                        class="my-2 mx-1 w-100 badge btn btn-secondary"
+                        class="my-2 mx-1  badge btn btn-secondary"
                         :class="category.attivo ? 'cate-attiva' : ''"
                         @click="sendCategory(category.id)">{{category.nome}}
                 </button>
@@ -55,33 +55,40 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-.container {
-    width: 40%;
+.backg-color {
+    margin-top: 50px;
 }
 
+.badge-div{
+    width: 60%;
+    margin: 0 auto;
+    display: block;
+}
 .badge {
     width: 100%;
     color: #00CDBE;
     border: 1px solid #00CDBE;
     border-radius: 20px;
     margin-top: 5px;
-    padding: 1rem 3rem 1rem 3rem;
-    font-size: 1rem;
+    padding: 15px 10px;
+    font-size: 20px;
     background-color: white;
-
 
     &:hover {
         color: white;
         background-color: #00CDBE;
-        ;
     }
 }
+
 .cate-attiva{
     color: white;
     background-color: #00CDBE;
+    border: solid 3px rgb(68, 0, 99);
 }
 
 .titolo {
     color: rgb(68, 0, 99);
+    margin-bottom: 30px;
+    font-style: italic;
 }
 </style>
