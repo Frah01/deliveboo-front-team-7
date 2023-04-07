8b9ca9313179108c51fffc4d7fa7dfb05d71719c
Frah01 <vinario.francesco@gmail.com>
2023-04-07 11:27:51 +0200

    Modifiche stile cart e responsive

--- src/components/AppCart.vue
+++ src/components/AppCart.vue
@@ -79,79 +79,104 @@ export default {
 <template lang="">
     <div class="container" >
         <div class="row" >
-            <h2 class="mt-5 text-center">CARRELLO</h2>
-            <div class="col-12">
-                <div class="card p-3 flow shadow custom-border mt-3 mb-5" >
-                    <div v-for="dish in dishes">
-                        <div class=" mb-3" v-if="dish.quantita != 0">
-                            <div>
-                                <div class="row g-0">
-                                    <div class="col-md-4">
-                                        <div v-if="dish.immagine.includes('dish_image')">
-                                                <img class="card-img-top rounded shadow" :src="`${store.baseUrl}/storage/${dish.immagine}`">
-                                        </div>
-                                        <div v-else>
-                                            <img class="card-img-top rounded shadow" :src="dish.immagine != null ? `${dish.immagine}`: 'https://artsmidnorthcoast.com/wp-content/uploads/2014/05/no-image-available-icon-6.png'" alt="">
+            <div class="card my-5 shadow px-0" >
+                <div class="card-header bg-header" >
+                    <h2 class="text-center text-white mb-0">Carrello</h2>
+                </div>
+                <div class="card-body" >
+                    <div class="col-12">
+                        <div class="card p-3 flow border-0 mt-3" >
+                            <div v-for="dish in dishes">
+                                <div class=" mb-3" v-if="dish.quantita != 0">
+                                    <div>
+                                        <div class="row g-0">
+                                            <div class="col-md-4">
+                                                <div v-if="dish.immagine.includes('dish_image')">
+                                                        <img class="card-img-top rounded shadow" :src="`${store.baseUrl}/storage/${dish.immagine}`">
+                                                </div>
+                                                <div v-else>
+                                                    <img class="card-img-top rounded shadow" :src="dish.immagine != null ? `${dish.immagine}`: 'https://artsmidnorthcoast.com/wp-content/uploads/2014/05/no-image-available-icon-6.png'" alt="">
+                                                </div>
+                                            </div>
+                                            <div class="col-md-8">
+                                                <div class="card-body p-3 ">
+                                                    <p class="fw-semibold "><span>{{ dish.nome }}</span></p>
+                                                    <p class="fw-semibold mb-0">Prezzo: <span>{{ dish.prezzo }} &euro;</span><span v-if="this.payment">  x {{ dish.quantita }}</span></p>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div class="col-md-8">
-                                        <div class="card-body p-3 ">
-                                            <p class="fw-semibold "><span>{{ dish.nome }}</span></p>
-                                            <p class="fw-semibold mb-0">Prezzo: <span>{{ dish.prezzo }} &euro;</span><span v-if="this.payment">  x {{ dish.quantita }}</span></p>
+                                    <div class="row">
+                                        <div class="col-4" >
+                                            <div class="d-flex justify-content-between align-items-center my-3" >
+                                                <button class="btn btn-sm indietro text-white fw-semibold" :disabled="dish.quantita == 0" @click="togliQuantita(dish)"><i class="fa-solid fa-minus"></i></button>
+                                                <span class="fw-semibold">{{ dish.quantita }}</span>
+                                                <button class="btn btn-sm indietro text-white fw-semibold" @click="aggiungiQuantita(dish)"><i class="fa-solid fa-plus"></i></button>
+                                            </div>
+                                        </div>
+                                        <div class="col-8" >
+                                            <div class="d-flex justify-content-end align-items-center my-3">
+                                                <button class="btn btn-sm btn-danger  text-white fw-semibold mx-2" @click="ClearItem(dish)"><i class="fa-solid fa-trash-can" title="Elimina"></i></button>
+                                            </div>
                                         </div>
                                     </div>
+                                    <hr class="border border-secondary border-1 opacity-75">
                                 </div>
                             </div>
-                            <div class="row">
-                                <div class="col-4" >
-                                    <div class="d-flex justify-content-between align-items-center my-3" >
-                                        <button class="btn btn-sm indietro text-white fw-semibold" :disabled="dish.quantita == 0" @click="togliQuantita(dish)"><i class="fa-solid fa-minus"></i></button>
-                                        <span class="fw-semibold">{{ dish.quantita }}</span>
-                                        <button class="btn btn-sm indietro text-white fw-semibold" @click="aggiungiQuantita(dish)"><i class="fa-solid fa-plus"></i></button>
-                                    </div>
-                                </div>
-                                <div class="col-8" >
-                                    <div class="d-flex justify-content-end align-items-center my-3">
-                                        <button class="btn btn-sm btn-danger  text-white fw-semibold mx-2" @click="ClearItem(dish)"><i class="fa-solid fa-trash-can" title="Elimina"></i></button>
-                                    </div>
+                            <div class="img-container" v-if="this.current_restaurant_id == this.storage_restaurant_id">
+                                <div v-if="this.prezzoTotale() == 0" >
+                                    <img class="object-fit-contain empty-cart-img"  src="../../public/empty-cart.png" alt="">
                                 </div>
                             </div>
-                            <hr class="border border-secondary border-1 opacity-75">
+                            <div v-else>
+                                <div class="img-container" v-if="!this.storage">
+                                    <img class="object-fit-contain empty-cart-img"  src="../../public/empty-cart.png" alt="">
+                                </div>
+                                <div v-else>
+                                    <p>Vuoi iniziare con un nuovo ordine?</p>
+                                    <button class="btn btn-sm indietro text-white fw-semibold me-2" @click="nuovoOrdine">Si</button>
+                                    <button class="btn btn-sm btn-danger text-white fw-semibold">No</button>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div v-if="this.current_restaurant_id == this.storage_restaurant_id">
-                        <div v-if="this.prezzoTotale() != 0" >
+                    </div>  
+                </div>
+                <div class="card-footer backg-body" v-if="this.prezzoTotale() != 0">
+                    <div class="d-flex justify-content-between align-items-center" >
+                        <div>
+                            <router-link :to="{ name: 'form-payments'}"  class="btn btn-sm indietro text-white fw-semibold me-2" title="Paga">Paga</router-link>
+                            <button type="submit" class="btn btn-sm btn-danger fw-semibold " @click="Clear()" title="Svuota Carrello">Svuota</button>
+                        </div>
+                        <div>
                             <p class="fw-semibold mb-0">Prezzo totale: <span>{{ prezzoTotale() }} &euro;</span></p>
                         </div>
                     </div>
-                    <div v-else>
-                        <div v-if="!this.storage">
-                            <img class="object-fit-contain empty-cart-img"  src="../../public/empty-cart.png" alt="">
-                        </div>
-                        <div v-else>
-                            <p>Vuoi iniziare con un nuovo ordine?</p>
-                            <button class="btn btn-sm indietro text-white fw-semibold me-2" @click="nuovoOrdine">Si</button>
-                            <button class="btn btn-sm btn-danger text-white fw-semibold">No</button>
-                        </div>
-                    </div>
                 </div>
-                <div class="mb-3" v-if="this.prezzoTotale() != 0">
-                    <router-link :to="{ name: 'form-payments'}"  class="btn btn-sm indietro text-white fw-semibold me-2" title="Paga">Paga</router-link>
-                    <button type="submit" class="btn btn-sm btn-danger fw-semibold " @click="Clear()" title="Svuota Carrello">Svuota</button>
-                </div>
-            </div>  
+            </div>
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
 .flow {
-    height: 50vh;
+    height: 60vh;
     overflow-y: auto;
 }
 
+.img-container{
+    overflow: hidden;
+}
 .empty-cart-img {
     width: 100%;
-    height: 45vh;
+    height: 50vh;
+    scale: 2.5;
 }
+
+.bg-header{
+    background-color: #00CDBE;
+}
+.backg-body{
+    background-color: rgba(209, 235, 153, 0.25);
+}
+
 </style>
--- src/pages/ThankYou.vue
+++ src/pages/ThankYou.vue
@@ -1,20 +1,134 @@
 <script>
+
 export default {
-    
+    data(){
+        return{
+            loading: true
+        }
+    },
+    methods: {
+        loader(){
+            setTimeout(() => {
+                this.loading = false
+            }, 3000) 
+        }
+    },
+    mounted(){
+        this.loader();
+        localStorage.clear();
+    }
 }
+
 </script>
 <template lang="">
     <div>
-        <div class="container" >
-            <div class="row" >
-                <div class="col-12 mt-5" >
-                    <h1 class="mt-5">Grazie per averci contattato!</h1>
-                    <router-link :to="{ name : 'homepage'}" class="btn btn-sm btn-primary" >Torna alla Homepage</router-link>
-                </div>
+        <div v-if="loading" class="container-loader">
+            <h1>Stiamo inviando la richiesta...</h1>
+            <div class="loader">
             </div>
+        </div>
+        <div v-else class="container-content">
+            <h1 class="grazie">Grazie per averci contattato!</h1>
+            <p>Abbiamo preso in carico la tua richiesta, a breve riceverai una mail di conferma con tutte le indicazioni!</p>
+            <router-link :to="{ name : 'homepage'}" class="bottone-thankyou" >Torna alla Homepage</router-link>
         </div>
     </div>
 </template>
-<style lang="">
+<style lang="scss" scoped>
+
+    .container-loader{
+        width: 100%;
+        height: 90vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-content: center;
+        align-items: center;
+
+        h1{
+            color: rgb(68, 0, 99) ;
+            font-style: italic;
+            margin-bottom: 30px;
+        }
+        .loader {
+            width: 80px;
+            height: 80px;
+            border-radius: 50%;
+            transition: 0.5;
+            border: 8px solid #d1914b;
+            --c:radial-gradient(farthest-side, #d64123 94%,#0000);
+            --b:radial-gradient(farthest-side, #000 94%,#0000);
+            background:
+                var(--c) 11px 15px,
+                var(--b) 6px 15px,    
+                var(--c) 35px 23px,
+                var(--b) 29px 15px,    
+                var(--c) 11px 46px,
+                var(--b) 11px 34px,    
+                var(--c) 36px 0px,
+                var(--b) 50px 31px,
+                var(--c) 47px 43px,
+                var(--b) 31px 48px,    
+                #f6d353; 
+            background-size: 15px 15px,6px 6px;
+            background-repeat: no-repeat;
+            animation: hu4 3s infinite;
     
+    
+            @keyframes hu4 {
+            0%     {-webkit-mask:conic-gradient(#0000 360deg     ,#000 0)}
+            16.67% {-webkit-mask:conic-gradient(#0000 300deg ,#000 0)}
+            33.33% {-webkit-mask:conic-gradient(#0000 240deg,#000 0)}
+            50%    {-webkit-mask:conic-gradient(#0000 180deg,#000 0)}
+            66.67% {-webkit-mask:conic-gradient(#0000 120deg,#000 0)}
+            83.33% {-webkit-mask:conic-gradient(#0000 60deg,#000 0)}
+            100%   {-webkit-mask:conic-gradient(#0000 0deg,#000 0)}
+            }
+    }
+}
+
+    .container-content{
+        width: 50%;
+        height: 90vh;
+        text-align: center;
+        margin: 0 auto;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-content: center;
+        align-items: center;
+
+        h1{
+            display: block;
+            color: rgb(68, 0, 99) ;
+            font-style: italic;
+            margin-bottom: 10px;
+        }
+
+        p{
+            font-size: 20px;
+            display: block;
+            color: rgb(68, 0, 99) ;
+            font-style: italic;
+            margin-bottom: 30px;
+        };
+
+        .bottone-thankyou {
+            text-decoration: none;
+            padding: 0.6em 1.2em;
+            margin-top: 20px;
+            border-radius: 10px;
+            cursor: pointer;
+            color: white;
+            border: none;
+            background: linear-gradient(120deg, darkmagenta, #00CDBE, #D0EB99);
+            background-size: 200% 100%;
+            background-position: 100% 0;
+            transition: background-position .5s;
+
+            &:hover {
+                background-position: 0 0;
+            }
+        }
+}
 </style>
