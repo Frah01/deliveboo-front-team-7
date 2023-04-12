ed37e8a58a524c4bbcd64f1684b8d03e5ab5cf95
giuseppemaisto <gmaisto98@gmail.com>
2023-04-12 11:12:50 +0200

    close #68

---
+++ src/pages/MyChart.vue
@@ -0,0 +1,38 @@
+<script>
+import Chart from 'chart.js/auto'
+export default {
+   mounted(){
+    const ctx = document.getElementById('myChart');
+
+const MyChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+MyChart;
+
+   } 
+}
+</script>
+<template lang="">
+    <div>
+        <canvas id="myChart"></canvas> 
+    </div>
+</template>
+
+<style lang="">
+    
+</style>
