3397af91ef1be914dffce60f0f9136983387ed94
alessiam1909 <ale.menon.vr@gmail.com>
2023-04-12 12:11:18 +0200

    close #70

--- src/pages/ChiSiamo.vue
+++ src/pages/ChiSiamo.vue
@@ -4,14 +4,45 @@ export default {
 }
 </script>
 <template lang="">
-        <div class="container">
-            <div class="row" >
-                <div class="col-12 mt-5">
-                    <h2 class="text-center mt-5">Chi siamo</h2>
+        <div class="container-chisiamo">
+            <div class="row row-chisiamo" >
+                <div class="col-12 mt-5 text-center">
+                    <img src="https://cdn-icons-png.flaticon.com/128/4834/4834940.png" alt="">
+                    <h2 class="mt-5">La storia di Deliveroo</h2>
+                    <p>Quando il fondatore e CEO di Deliveroo Will Shu si è trasferito a Londra nel 2013, ha trovato una città piena di ristoranti incredibili, ma è rimasto stupito che davvero pochi di questi consegnassero a domicilio. Così, ha deciso che la sua missione personale sarebbe stata quella di portare i migliori ristoranti della città direttamente nelle case delle persone. Oggi Deliveroo è al centro di un marketplace che mette in connessione tre attori: i clienti, i ristoranti e i supermercati, e i rider. L’obiettivo principale di questa connessione è facilitare un’esperienza di acquisto indimenticabile, in circa 30 minuti. Offrendo una consegna veloce e affidabile, che il cliente può seguire passo passo dallo smartphone, Deliveroo ha visto una crescita rapida e sostenuta. Oggi Deliveroo è presente in 11 mercati in tutto il mondo, collabora con migliaia di ristoranti, supermercati e rider e arriva nelle case di milioni di clienti.</p>
+                </div>
+                <div class="col-12 mt-5 text-center">
+                    <img src="https://cdn-icons-png.flaticon.com/128/7217/7217491.png" alt="">
+                    <h2 class="mt-5">Innovazione, a domicilio</h2>
+                    <p>La passione di Deliveroo è il cibo, con l’obiettivo di fornire alle persone una scelta sempre più ampia. Ma la missione di Deliveroo non è solo quella di far sì che i ristoranti e supermercati della tua zona siano a tua disposizione dove vuoi e quando vuoi; vogliamo spingerci più in là, portando nuovi ristoranti vicino a te perché tu possa avere ancora più scelta quando si tratta di mangiare bene.  Deliveroo sta inoltre investendo nell’espansione del proprio servizio di spesa “on demand” offrendo la consegna dalle catene di supermercati più conosciute a livello nazionale, ma anche fornendo la consegna rapida dalla nostra rete di negozi dedicati esclusivamente al delivery, che si chiama Deliveroo HOP.</p>
+                </div>
+                <div class="col-12 mt-5 text-center">
+                    <img src="https://cdn-icons-png.flaticon.com/128/8336/8336130.png" alt="">
+                    <h2 class="mt-5">I nostri valori</h2>
+                    <p>I nostri valori sono al centro di tutto l’operato di Deliveroo. Ci guidano ogni giorno per lavorare in squadra e prendere decisioni, dalle scelte che hanno un impatto sulle assunzioni e sullo sviluppo del personale, a quelle che scrivono il futuro dei prodotti Deliveroo.</p>
                 </div>
             </div>
         </div>
 </template>
-<style lang="">
-    
+<style lang="scss" scoped>
+    .container-chisiamo{
+        background-image: url('/public/Video senza titolo - Realizzato con Clipchamp (1).gif');
+       
+
+        .row-chisiamo{
+            width: 60%;
+            margin: 0 auto;
+            padding: 50px ;
+
+            h2{
+                color: rgb(0, 205, 190);
+                font-weight: 600;
+                text-shadow: 2px 2px 2px rgb(68, 0, 99) ;
+            }
+
+            p{
+                color: rgb(68, 0, 99) ;
+            }
+        }
+    }
 </style>
